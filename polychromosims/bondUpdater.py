c7c406c54ece983758e36bbcddd1821303e29128
Simon Grosse-Holz <sgh256@mit.edu>
2019-10-21 14:59:24 -0400

    automatic update

--- polychromosims/bondUpdater.py
+++ polychromosims/bondUpdater.py
@@ -43,8 +43,9 @@ class bondUpdater(object):
         #precalculating all bonds
         allBonds = []
         
+        if self.curtime + blocks > self.LEFpositions.shape[0]:
+            blocks = self.LEFpositions.shape[0] - self.curtime
         loaded_positions  = self.LEFpositions[self.curtime : self.curtime+blocks]
-        sys.err('loaded_positions.shape = {}'.format(loaded_positions.shape))
         allBonds = [[(int(loaded_positions[i, j, 0]), int(loaded_positions[i, j, 1])) 
                         for j in range(loaded_positions.shape[1])] for i in range(blocks)]
 
